601f0fd0ceffc8a8be1fa0229eb1525e17847086
DormunDoorlock <skrudgh2001@gmail.com>
2021-03-15 21:01:47 +0900

    added society score

--- report_card.c
+++ report_card.c
@@ -4,10 +4,10 @@ int main(){
 	int korea=90;
 	int math=80;
 	int english=100;
-
+        int society=95;
 	printf("Korean=%d\n",korea);
 	printf("Math=%d\n",maht);
 	printf("English=%d\n",english);
-
+        printf("Society=%d\n",society);
 	return 0;
 }
