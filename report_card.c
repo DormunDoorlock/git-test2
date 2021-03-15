805108c11221309cbbf175cfdb366db6a5f35359
DormunDoorlock <skrudgh2001@gmail.com>
2021-03-15 20:43:21 +0900

    report card:Add base code

---
+++ report_card.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	int korea=90;
+	int math=80;
+	int english=100;
+
+	printf("Korean=%d\n",korea);
+	printf("Math=%d\n",maht);
+	printf("English=%d\n",english);
+
+	return 0;
+}
