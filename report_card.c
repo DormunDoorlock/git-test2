3594eecc42704e12f3f328045e2921634035a3f8
DormunDoorlock <skrudgh2001@gmail.com>
2021-03-15 20:57:59 +0900

    added science score

--- report_card.c
+++ report_card.c
@@ -4,10 +4,10 @@ int main(){
 	int korea=90;
 	int math=80;
 	int english=100;
-
+        int science=100;
 	printf("Korean=%d\n",korea);
 	printf("Math=%d\n",maht);
 	printf("English=%d\n",english);
-
+        printf("Science=%d\n",science);
 	return 0;
 }
